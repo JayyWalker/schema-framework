e1f3630f073206600929ab2b47616b192244f2fb
Jayy <jordan@jayy.online>
2023-09-11 22:29:55 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { createServer } from 'http'
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.end('hello world')
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

6337a060d6fdb583878f500af55f7c81a02d381f
Jayy <jordan@jayy.online>
2023-09-12 02:12:37 +0100

    route regex handler

---
+++ src/router/route-regex.test.ts
@@ -0,0 +1,77 @@
+import { describe, expect, it } from 'vitest'
+import { convertParameterConstraint, createRouteRegex, ParameterConstraintError, parameterToRegex } from './route-regex'
+
+describe('route regexes', () => {
+  describe('convert parameter constraint', () => {
+    it.each([
+      ['int', '\\d+'],
+      ['unknown', '.+'],
+    ])('should convert %s string to regex %s', (humanConstraint, expected) => {
+      const sut = convertParameterConstraint(humanConstraint)
+
+      expect(sut).toBe(expected)
+    })
+
+    it('should throw error for invalid field types provided', () => {
+      expect(() => {
+        convertParameterConstraint('foo')
+      }).toThrowError(ParameterConstraintError)
+
+      expect(() => {
+        convertParameterConstraint('foo')
+      }).toThrow('Unrecognised field type provided as parameter constraint')
+    })
+  })
+
+  describe('converts path segment to regex', () => {
+    it('should return segment to sender if no regex is recognised', () => {
+      const sut = parameterToRegex('author')
+
+      expect(sut).toBe('author')
+    })
+
+    it('should return regex with constraint', () => {
+      const sut = parameterToRegex('{id:int}')
+
+      expect(sut).toBe('(?<id>\\d+)')
+    })
+
+    it('should return regex without a constraint', () => {
+      const sut = parameterToRegex('{id}')
+
+      expect(sut).toBe('(?<id>.+)')
+    })
+  })
+
+  describe('create regex version of pathname', () => {
+    it('should return regex for /', () => {
+      const sut = createRouteRegex('/')
+
+      expect(sut).toBe('^\\/$')
+    })
+
+    it('should return regex with no path parameters', () => {
+      const sut = createRouteRegex('/author')
+
+      expect(sut).toBe('^\\/author\\/$')
+    })
+
+    it('should return regex with no constraints', () => {
+      const sut = createRouteRegex('/book/{id}')
+
+      expect(sut).toBe('^\\/book\\/(?<id>.+)\\/$')
+    })
+
+    it('should return regex for full pathname', () => {
+      const sut = createRouteRegex('/author/{id:int}')
+
+      expect(sut).toBe('^\\/author\\/(?<id>\\d+)\\/$')
+    })
+
+    it('should return regex with end slash when pathname did not provide end slash', () => {
+      const sut = createRouteRegex('/author/{id:int}/')
+
+      expect(sut).toBe('^\\/author\\/(?<id>\\d+)\\/$')
+    })
+  })
+})
---
+++ src/router/route-regex.ts
@@ -0,0 +1,50 @@
+/**
+ * Converts Human-readable constraint to a regex
+ * @param humanConstraint
+ */
+export function convertParameterConstraint (humanConstraint: string): string {
+  switch(humanConstraint) {
+    case('int'):
+      return '\\d+'
+    case('unknown'):
+      return '.+'
+    default:
+      throw new ParameterConstraintError('Unrecognised field type provided as parameter constraint')
+  }
+}
+
+export function parameterToRegex (routeSegments: string) {
+  // Checks if the segment is a parameter. If not, it is returned as normal
+  if (routeSegments.indexOf('{') < 0) {
+    return routeSegments
+  }
+
+  const parameter: string[] = routeSegments
+    .substring(1, (routeSegments.length - 1))
+    .split(':')
+
+  const [parameterName, constraint] = parameter
+
+  const parameterConstraintRegex: string = parameter.length === 2
+    // A constraint is provided
+    ? convertParameterConstraint(constraint)
+    // When no constraint is provided
+    : convertParameterConstraint('unknown')
+
+  return `(?<${parameterName}>${parameterConstraintRegex})`
+}
+
+export function createRouteRegex(routePath: string): string {
+  let result = routePath
+    .split('/')
+    .map(parameterToRegex)
+    .join('\\/')
+
+  if (!result.endsWith('/')) {
+    result = `${result}\\/`
+  }
+
+  return `^${result}$`
+}
+
+export class ParameterConstraintError extends Error {}
